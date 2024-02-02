0bcde150f5634a659f2989784550e3aee5fc7e53
Yukun Xie <yxie@lcs.on.ca>
2024-02-02 08:53:52 -0500

    Initial Commit

---
+++ Arithmetic Buddy/Arithmetic_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_BuddyApp.swift
+//  Arithmetic Buddy
+//
+//  Created by Yukun Xie on 2024/2/2.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
